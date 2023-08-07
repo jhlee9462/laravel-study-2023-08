1582015dd61513e7b944bd4f89589db69b6ec3f7
jhlee <jhlee9462@github.com>
2023-08-07 13:05:22 +0900

    add requirement username when sign-up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,35 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img
+                src="/jpeg/doori.jpeg"
+                alt="프로필 사진"
+                class="rounded-circle w-75 ">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>jhlee9462</h1></div>
+            <div class="d-flex">
+                <div class="pe-5"><strong>153</strong> posts</div>
+                <div class="pe-5"><strong>23k</strong> followers</div>
+                <div class="pe-5"><strong>212</strong> following</div>
             </div>
+            <div class="pt-4 fw-bold">github.com/jhlee9462</div>
+            <div>Hi! I'm passionate developer Jinho.</div>
+            <div><a href="#">github.com/jhlee9462</a></div>
+        </div>
+    </div>
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="/jpeg/doori.jpeg" alt="" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="/jpeg/doori.jpeg" alt="" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="/jpeg/doori.jpeg" alt="" class="w-100">
         </div>
     </div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>JhleeGram</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
@@ -21,7 +21,8 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <div><img src="/svg/freeCodeCampLogo.svg" alt="Logo"></div>
+                    <div>JhleeGram</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -51,7 +52,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                                    {{ Auth::user()->username }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
