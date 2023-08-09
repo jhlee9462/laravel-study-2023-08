bd1328460562cf1599d53e308717197e8dfd3a2b
jhlee <jhlee9462@github.com>
2023-08-09 13:42:42 +0900

    add edit profile

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -1,40 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-3 p-5">
-            <img
-                src="/jpeg/doori.jpeg"
-                alt="프로필 사진"
-                class="rounded-circle w-75 ">
-        </div>
-        <div class="col-9 pt-5">
-            <div class="d-flex justify-content-between align-items-baseline">
-                <h1>{{ $user->username }}</h1>
-                <a href="/p/create">Add New Post</a>
+    <div class="container">
+        <div class="row">
+            <div class="col-3 p-5">
+                <img
+                    src="/jpeg/doori.jpeg"
+                    alt="프로필 사진"
+                    class="rounded-circle w-75 ">
             </div>
-            <div class="d-flex">
-                <div class="pe-5"><strong>153</strong> posts</div>
-                <div class="pe-5"><strong>23k</strong> followers</div>
-                <div class="pe-5"><strong>212</strong> following</div>
+            <div class="col-9 pt-5">
+                <div class="d-flex justify-content-between align-items-baseline">
+                    <h1>{{ $user->username }}</h1>
+                    @can('update', $user->profile)
+                        <a href="/p/create">Add New Post</a>
+                    @endcan
+                </div>
+                @can('update', $user->profile)
+                    <a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
+                @endcan
+                <div class="d-flex">
+                    <div class="pe-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
+                    <div class="pe-5"><strong>23k</strong> followers</div>
+                    <div class="pe-5"><strong>212</strong> following</div>
+                </div>
+                <div class="pt-4 fw-bold">{{ $user->profile->title }}</div>
+                <div>{{ $user->profile->description }}</div>
+                <div><a href="{{ $user->profile->url }}">{{ $user->profile->url }}</a></div>
             </div>
-            <div class="pt-4 fw-bold">{{ $user->profile->title }}</div>
-            <div>{{ $user->profile->description }}</div>
-            <div><a href="{{ $user->profile->url }}">{{ $user->profile->url }}</a></div>
         </div>
-    </div>
 
-    <div class="row pt-5">
-        <div class="col-4">
-            <img src="/jpeg/doori.jpeg" alt="" class="w-100">
-        </div>
-        <div class="col-4">
-            <img src="/jpeg/doori.jpeg" alt="" class="w-100">
-        </div>
-        <div class="col-4">
-            <img src="/jpeg/doori.jpeg" alt="" class="w-100">
+        <div class="row pt-5">
+            @foreach($user->posts as $post)
+                <div class="col-4 pb-4">
+                    <a href="/p/{{ $post->id }}">
+                        <img src="/storage/{{ $post->image }}" alt="" class="w-100">
+                    </a>
+                </div>
+            @endforeach
         </div>
     </div>
-</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,8 @@ Auth::routes();
 
 Route::get('/p/create', [PostsController::class, 'create']);
 Route::post('/p', [PostsController::class, 'store']);
+Route::get('/p/{post}', [PostsController::class, 'show']);
 
 Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
+Route::get('/profile/{user}/edit', [App\Http\Controllers\ProfilesController::class, 'edit'])->name('profile.edit');
+Route::patch('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'update'])->name('profile.update');
